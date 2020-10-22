4f953627f6c3df9986094e946f675aa9c0853dba
jinalshah999 <jinalshah999@gmail.com>
2020-10-22 08:01:35 +0530

    added routes for tasks

--- Models/Task.js
+++ Models/Task.js
@@ -5,6 +5,15 @@ var Task = {
     getAllTasks: function (callback) {
 
         return db.query('select * from task', callback);
+    },
+    addTask: function (task, callback) {
+        return db.query('insert into task values(?,?,?)', [task.Id, task.Title, task.Status], callback);
+    },
+    deleteTask: function (id, callback) {
+        return db.query('delete from task where Id=?', [id], callback);
+    },
+    updateTask: function (id, task, callback) {
+        return db.query('update task set Title=?,Status=? where Id=?', [task.Title, task.Status, id], callback);
     }
 
 };
--- routes/task_router.js
+++ routes/task_router.js
@@ -15,5 +15,43 @@ router.get('/', function (req, res, next) {
     });
 
 });
+//http://localhost:3000/tasks/
+router.post('/', function (req, res, next) {
 
+    task.addTask(req.body, function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+
+    });
+});
+//http://localhost:3000/tasks/1
+router.delete('/:id', function (req, res, next) {
+
+    task.deleteTask(req.params.id, function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+
+});
+//http://localhost:3000/tasks/1
+router.put('/:id', function (req, res, next) {
+
+    task.updateTask(req.params.id, req.body, function (err, rows) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(rows);
+        }
+    });
+
+});
 module.exports = router;
